2bfddc6828b5f169f5b30dcc45885f2bab84f403
Debjit Ghosh <debjit.prof@gmail.com>
2024-08-18 17:24:06 +0530

    before compiling tsc

--- lib/cdk_demo_lambda-stack.ts
+++ lib/cdk_demo_lambda-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { CfnBucket, Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class CdkDemoLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambda = new Function(this, "demo-cdk-lambda", {
+      runtime: Runtime.NODEJS_20_X,
+      code: Code.fromAsset(path.join(__dirname, "resources")),
+      handler: "demoLambda.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const apiGateway = new RestApi(this, "demo-api-gateway", {
+      restApiName: "dummy-api",
+    });
+
+    const lambdaIntegration = new LambdaIntegration(lambda, { proxy: true });
+
+    const resource = apiGateway.root.addResource("hello");
+    resource.addMethod("GET", lambdaIntegration);
   }
 }
