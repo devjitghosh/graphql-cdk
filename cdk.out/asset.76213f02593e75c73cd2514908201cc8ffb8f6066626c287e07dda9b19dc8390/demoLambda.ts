92e6db7efc76a266f4b21eae63d3aabc43e0cede
Debjit Ghosh <debjit.prof@gmail.com>
2024-08-25 21:55:55 +0530

    cdk

---
+++ cdk.out/asset.76213f02593e75c73cd2514908201cc8ffb8f6066626c287e07dda9b19dc8390/demoLambda.ts
@@ -0,0 +1,8 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+exports.handler = async (event: APIGatewayProxyEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "successful" }),
+  };
+};
